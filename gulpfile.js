d728746c3be04008f189a7f2fbd3a35571e7e4e5
Rose <TheBarBreaker@gmail.com>
2017-10-11 23:12:34 -0400

    added autoprefixer, updated gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,11 @@
 const gulp = require('gulp'),
-imagemin = require('gulp-imagemin'),
-mini = require('gulp-uglify'),
-sass = require('gulp-sass'),
-cat = require('gulp-concat'),
-norm = require('node-normalize-scss'),
-bs = require('browser-sync').create();
+  imagemin = require('gulp-imagemin'),
+  mini = require('gulp-uglify'),
+  sass = require('gulp-sass'),
+  cat = require('gulp-concat'),
+  norm = require('node-normalize-scss'),
+  bs = require('browser-sync').create(),
+  autoprefixer = require('gulp-autoprefixer');
 
 /*TOP LEVEL FUNCTIONS REMINDER
 gulp.task - DEFINE TASKS
@@ -14,58 +15,58 @@ gulp.watch - WATCH FILES & FOLDERS FOR CHANGES
 */
 
 // [gulp] - runs my default array of tasks
-gulp.task('default', ['say', 'copyhtml', 'scripts', 'imagemin', 'sass']);
+gulp.task('default', ['copyhtml', 'scripts', 'imagemin', 'sass']);
 
 // [watch] - watches specified files for changes & runs tasks attached
-gulp.task('watch', function(){
-    bs.init({
-      server: {
-        baseDir: "dist"
-      }
-    });
+gulp.task('watch', function() {
+  bs.init({
+    server: {
+      baseDir: "dist"
+    }
+  });
   gulp.watch('src/scripts/*.js', ['scripts']);
   gulp.watch('src/assets/images/*', ['imagemin']);
   gulp.watch('src/assets/sass/**/*.scss', ['sass']);
   gulp.watch('src/*.html', ['copyhtml']);
-  gulp.watch('dist/*.html', function(){
+  gulp.watch('dist/*.html', function() {
     bs.reload()
   });
-  // gulp.watch('dist/*.css', [])
 });
 
-gulp.task('sassqwatch', function(){
+gulp.task('sassqwatch', function() {
   gulp.watch('src/assets/sass/*.scss', ['sass']);
-});
-
-// [say] - Logs Message
-gulp.task('say', function(){
-  return console.log('Gulp is running...');
 });
 
 // [copyhtml] - Copies files from one location to another
 gulp.task('copyhtml', () =>
   gulp.src('src/*.html')
-    .pipe(gulp.dest('dist'))
+  .pipe(gulp.dest('dist'))
 );
 
 // [imagemin] - Optimizes images
 gulp.task('imagemin', () =>
   gulp.src('src/assets/images/*')
-    .pipe(imagemin())
-    .pipe(gulp.dest('dist/assets/images'))
+  .pipe(imagemin())
+  .pipe(gulp.dest('dist/assets/images'))
 );
 
 // [mini] - Minify jscripts
 gulp.task('mini', () =>
   gulp.src('src/scripts/*.js')
-    .pipe(mini())
-    .pipe(gulp.dest('dist/scripts'))
+  .pipe(mini())
+  .pipe(gulp.dest('dist/scripts'))
 );
 
-// [sass] - Runs CSS compiler through SASS
+// [sass] - Runs Sass & autopfx, injects CSS into browsersync
 gulp.task('sass', function() {
   return gulp.src('src/assets/sass/*.scss')
-    .pipe(sass({includePaths: norm.includePaths}).on('error', sass.logError))
+    .pipe(sass({
+      includePaths: norm.includePaths
+    }).on('error', sass.logError))
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions'],
+      cascade: false
+    }))
     .pipe(gulp.dest('dist/assets/css'))
     .pipe(bs.stream());
 });
@@ -77,9 +78,3 @@ gulp.task('scripts', () =>
   .pipe(mini())
   .pipe(gulp.dest('dist/js'))
 );
-
-//
-// gulp.task('cssInject', ['sass'], () => {
-//   return gulp.src('/dist/assets/css/styles.css')
-//     .pipe(bs.stream());
-// });
