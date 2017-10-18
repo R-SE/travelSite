a1152a7a23f0b0f87c2b7134fb44d107ba44e2fe
Rose <TheBarBreaker@gmail.com>
2017-10-17 20:10:20 -0400

    installed waypoints module and set up reveal-on-scroll class

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,4 +1,6 @@
 const $ = require('jquery');
 import MobileMenu from './modules/mobile-menu';
+import RevealOnScroll from './modules/reveal-on-scroll';
 
 var mobileMenu = new MobileMenu();
+var revealOnScroll = new RevealOnScroll();
